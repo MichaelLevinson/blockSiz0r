74ec593a7d71793fe6ef655ce999accbacce810c
MichaelLevinson <MichaelLevinson2013@u.northwestern.edu>
2025-04-02 08:59:09 -0700

    Create getSizes.js
    Used to get initcode, and byteCode sizes
    
    node getSizes.js 
    
    put in root directory, contract goes in contractPath

---
+++ getSizes.js
@@ -0,0 +1,42 @@
+// getSizes.js
+// Michael Levinson - 2025-04-02 
+// This script is used to get the size of the bytecode and initcode of a contract
+
+const solc = require('solc');
+const fs = require('fs');
+const path = require('path');
+
+function getContractSizes(contractPath) {
+  // Read source
+  const source = fs.readFileSync(contractPath, 'utf8');
+  
+  // Compile
+  const input = {
+    language: 'Solidity',
+    sources: { 
+      'GaslightDungeon.sol': { content: source } 
+    },
+    settings: { 
+      outputSelection: { 
+        '*': { '*': ['evm.bytecode', 'evm.deployedBytecode'] }
+      } 
+    }
+  };
+  
+  const output = JSON.parse(solc.compile(JSON.stringify(input)));
+  
+  // Get contract data
+  const contractName = 'Enter Contract Name Here'; // Using your contract name
+  const contract = output.contracts['Enter Contract Name Here.sol'][contractName]; // Using your contract name
+  
+  // Get bytecode sizes
+  const initcodeSize = contract.evm.bytecode.object.length / 2;
+  const bytecodeSize = contract.evm.deployedBytecode.object.length / 2;
+  
+  return { initcodeSize, bytecodeSize };
+}
+
+const contractPath = path.resolve('./contracts/Enter Contract Name Here.sol'); // Using your contract name
+const sizes = getContractSizes(contractPath);
+console.log(`Initcode size: ${sizes.initcodeSize} bytes`);
+console.log(`Bytecode size: ${sizes.bytecodeSize} bytes`);
